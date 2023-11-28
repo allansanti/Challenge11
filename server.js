d4114c35bc037e2b200ac5e1c041d341760fb6d3
allansanti <allan.santi02@gmail.com>
2023-11-28 08:53:50 -0400

    Structure changed: code added

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+//client -> middlerware (pre-processing before give to the server app.use()) -> server
+// Import custom middleware, "cLog"
+
+
+// Middleware for parsing JSON and urlencoded form data
+// data parser in app.use to parse client data to req.body (object client data object)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// middleware to modulize api routes
+// break down routes into separate files --> allows for expansion
+// api base, first piece
+// http://localhost:3001/api 
+
+app.use('/api', api);
+
+//middleware to make public the homepage url http://localhost:3001
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
